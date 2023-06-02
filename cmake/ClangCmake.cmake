61927adbba4faac77bad09616c880e3e71fb19af
ErezAmihud <erezamihud@gmail.com>
2023-06-02 15:33:30 +0300

    fix target naming

--- cmake/ClangCmake.cmake
+++ cmake/ClangCmake.cmake
@@ -2,9 +2,9 @@ include(${CMAKE_CURRENT_LIST_DIR}/IWYU.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/ClangFormat.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/ClangTidy.cmake)
 
-function(taget_setup target)
+function(target_setup target)
     target_clangformat_setup(${target})
    target_clangtidy_setup(${target})
    target_iwyu_setup(${target})
        
-endfunction(taget_setup)
+endfunction(target_setup)
